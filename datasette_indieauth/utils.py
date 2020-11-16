84f530c0c94191d72ddca1f4fc6fdc7bc1e8971e
Simon Willison <swillison@gmail.com>
2020-11-16 09:58:53 -0800

    verify_client_identifier(), closes #7

--- datasette_indieauth/utils.py
+++ datasette_indieauth/utils.py
@@ -30,3 +30,36 @@ def verify_profile_url(url):
     except ValueError:
         pass
     return True
+
+
+def verify_client_identifier(url):
+    bits = urlparse(url)
+    # Client identifier URLs MUST have either an https or http scheme
+    if bits.scheme not in ("http", "https"):
+        return False
+    # MUST contain a path component (/ is a valid path)
+    if not bits.path:
+        return False
+    # MUST NOT contain single-dot or double-dot path segments
+    if "/./" in bits.path or "/../" in bits.path:
+        return False
+    # MAY contain a query string component
+    # MUST NOT contain a fragment component
+    if bits.fragment:
+        return False
+    # MUST NOT contain a username or password component
+    if "@" in bits.netloc:
+        return False
+    # MAY contain a port
+    # Additionally, hostnames MUST be domain names or a loopback interface
+    # and MUST NOT be IPv4 or IPv6 addresses except for
+    # IPv4 127.0.0.1 or IPv6 [::1].
+    if bits.netloc == "127.0.0.1":
+        return True
+    # TODO: Allow IPv6 [::1]
+    try:
+        ipaddress.ip_address(bits.netloc)
+        return False
+    except ValueError:
+        pass
+    return True
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -18,6 +18,7 @@ def test_verify_profile_url_valid(url):
     "url",
     [
         "example.com",
+        "http://example.com",
         "mailto:user@example.com",
         "https://example.com/foo/../bar",
         "https://example.com/#me",
@@ -28,3 +29,34 @@ def test_verify_profile_url_valid(url):
 )
 def test_verify_profile_url_invalid(url):
     assert not utils.verify_profile_url(url)
+
+
+@pytest.mark.parametrize(
+    "url",
+    [
+        "https://example.com/",
+        "https://example.com/username",
+        "https://example.com/users?id=100",
+        "https://example.com:8443/",
+        "http://127.0.0.1/",
+        "http://localhost/",
+    ],
+)
+def test_verify_client_identifier_valid(url):
+    assert utils.verify_client_identifier(url)
+
+
+@pytest.mark.parametrize(
+    "url",
+    [
+        "example.com",
+        "http://example.com",
+        "mailto:user@example.com",
+        "https://example.com/foo/../bar",
+        "https://example.com/#me",
+        "https://user:pass@example.com/",
+        "https://172.28.92.51/",
+    ],
+)
+def test_verify_client_identifier_invalid(url):
+    assert not utils.verify_client_identifier(url)
